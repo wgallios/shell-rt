d6a06bb2cc700d73155b716f2496d4d7dd216776
William Gallios <williamgallios@gmail.com>
2025-11-11 15:25:09 -0800

    setting up argparse

--- shell_next_cmd_lstm.py
+++ shell_next_cmd_lstm.py
@@ -8,7 +8,24 @@ from torch.utils.data import Dataset, DataLoader
 
 
 def main():
-    print("Test")
+    print("Shell Next Command Prediction with LSTM")
+
+    p = argparse.ArgumentParser(description="Train an LSTM model to predict the next shell command.")
+    sub = p.add_subparsers(dest="cmd")
+
+    t = sub.add_parser("train");
+    t.add_argument("--epochs", type=int, default=3)
+
+    args = p.parse_args()
+
+    if (args.cmd == "train"):
+        print("Start Training")
+    elif (args.cmd == "suggest"):
+        print("Start Suggesting")
+    else:
+        p.print_help()
+
+
 
 if __name__ == "__main__":
     main()
